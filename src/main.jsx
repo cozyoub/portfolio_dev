81c764892a4aaa593e98503210e794d14f761baa
DESKTOP-G407F77\it <am_o_o@naver.com>
2026-02-03 12:27:19 +0900

    Fix GitHub Pages routing with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -15,8 +15,8 @@ AOS.init({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio_dev">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
